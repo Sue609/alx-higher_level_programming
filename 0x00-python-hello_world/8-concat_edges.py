b21263b1cae02b24fecebcf3ca4c44f7bafb23df
Sue609 <susankamau609@gmail.com>
2023-05-08 11:05:33 -0700

    corrections

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str.split("with ")[1].rstrip(" syntax"))
+str = str[39:67] + str[109:112] + str[:6]
+print(str)
