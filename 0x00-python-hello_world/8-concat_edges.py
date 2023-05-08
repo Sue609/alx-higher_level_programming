9869efc8a917b52c21395417d25c205240c8b34e
Sue609 <susankamau609@gmail.com>
2023-05-08 10:15:31 -0700

    The Zen of Python, by Tim Peters

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str.split("with ")[1].rstrip(" syntax"))
