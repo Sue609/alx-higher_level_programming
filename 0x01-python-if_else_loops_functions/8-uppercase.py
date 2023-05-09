c6b022b5b99f2c0f271ba62a3d0a5f15d91221a1
Sue609 <susankamau609@gmail.com>
2023-05-09 04:08:11 -0700

    Write a function that prints the numbers from 1 to 100 separated by a space.

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,7 @@
 
 def uppercase(str):
     for c in str:
-        print("{:c}".format(ord(c) - 32 if 97 <= ord(c) <= 122 else ord(c)), end="")
+        print("{:c}".format(ord(c) - 32 if 97 <= ord(c) <= 122 
+        else ord(c)), end="")
 
     print()
