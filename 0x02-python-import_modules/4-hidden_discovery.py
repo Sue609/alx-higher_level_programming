08724bb9deb6bcb50ad9f23f2cb9dc2ba8f3c3f7
Sue609 <susankamau609@gmail.com>
2023-05-11 03:33:32 -0700

    Write a program that prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,16 +2,19 @@
 
 import dis
 import marshal
+if __name__ == "__main__":
 
-with open("hidden_4.pyc", "rb") as f:
-    code = marshal.load(f)
+    with open("hidden_4.pyc", "rb") as f:
+        code = marshal.load(f)
 
-names = set()
-for instr in dis.Bytecode(code):
-    if instr.opname == "LOAD_CONST":
-        const = instr.arg
-        if isinstance(const, str) and not const.startswith("__"):
-            names.add(const)
+    names = set()
+    for instr in dis.Bytecode(code):
 
-for name in sorted(names):
-    print(name)
+        if instr.opname == "LOAD_CONST":
+            const = instr.arg
+
+            if isinstance(const, str) and not const.startswith("__"):
+                names.add(const)
+
+        for name in sorted(names):
+            print(name)
