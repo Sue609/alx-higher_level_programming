797a79a81520fd78eadd3c8ed2c6b75973a2c68d
Sue609 <susankamau609@gmail.com>
2023-05-11 03:18:21 -0700

    Write a program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,9 +7,8 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     num_args = len(args)
 
-    print("{} arguement{}{}"
+    print("{} argument{}{}"
             .format(num_args, 's' if num_args != 1 else '', '.' if num_args == 0 else ':'))
 
     for i, arg in enumerate(args):
-        print("{}: {}"
-                .format(i + 1, arg))
+        print("{}: {}".format(i + 1, arg))
