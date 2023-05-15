07da2b747d16803d58a30f35aa4dada38a5d2d4b
Sue609 <susankamau609@gmail.com>
2023-05-15 10:01:58 -0700

    Write a function in C that checks if a singly linked list is a palindrome.

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,56 @@
+#include "lists.h"
+#include <stddef.h>
+/**
+ * is_palindrome - a function that checks if a number is a palinfrome.
+ * @head: a double pointer
+ *
+ * Return: an integer 1 or 0.
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *secondHalf = NULL;
+	listint_t *prev = NULL;
+	listint_t *temp = NULL;
+	int isPalindrome = 1;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (isPalindrome);
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		prev = slow;
+		slow = slow->next; }
+	if (fast != NULL)
+	{
+		secondHalf = slow->next;
+		slow->next = NULL; }
+	else
+	{
+		secondHalf = slow;
+		prev->next = NULL; }
+	while (secondHalf != NULL)
+	{
+		temp = secondHalf->next;
+		prev = secondHalf;
+		secondHalf = temp; }
+	secondHalf = prev;
+	temp = *head;
+	while (secondHalf != NULL)
+	{
+		if (temp->n != secondHalf->n)
+		{
+			isPalindrome = 0;
+			break; }
+		temp = temp->next;
+		secondHalf = secondHalf->next; }
+	prev = NULL;
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = temp; }
+	*head = prev;
+	return (isPalindrome); }
