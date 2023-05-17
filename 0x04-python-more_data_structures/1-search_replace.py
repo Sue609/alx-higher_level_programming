dd1f3211ed30008025add228cdf641faca0aad52
Sue609 <susankamau609@gmail.com>
2023-05-17 06:42:30 -0700

    python corrections

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-	new_list = []
+    new_list = []
 
-	for element in my_list:
+    for element in my_list:
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
 
-		if element == search:
-			new_list.append(replace)
-
-		else:
-			new_list.append(element)
-
-	return new_list
+    return new_list
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-	unique_nums = set()
-	total_sum = 0
+    unique_nums = set()
+    total_sum = 0
 
-	for num in my_list:
-		if num not in unique_nums:
-			total_sum += num
-			unique_nums.add(num)
+    for num in my_list:
+        if num not in unique_nums:
+            total_sum += num
+            unique_nums.add(num)
 
-	return total_sum
+    return total_sum
