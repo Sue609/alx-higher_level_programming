52a34f070979af5f2afe8dc944de15ff02e092c0
Sue609 <susankamau609@gmail.com>
2023-06-03 23:42:16 -0700

    Write a function that prints a square with the character #.

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def matrix_divided(matrix, div):
-    if not all(isinstance(row, list) and all(isinstance(val, (int, float)) for val in row) for row in matrix):
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-    row_size = len(matrix[0])
-    if not all(len(row) == row_size for row in matrix):
+    if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
 
@@ -16,10 +15,10 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-
     divided_matrix = [[round(val / div, 2) for val in row] for row in matrix]
 
-    return (divided_matrix)
+    return divided_matrix
+
 
 if __name__ == "__main__":
     import doctest
