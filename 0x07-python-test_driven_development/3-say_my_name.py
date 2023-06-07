117354e922c38988e15e977a7635a1a627c68a75
Sue609 <susankamau609@gmail.com>
2023-06-07 10:58:35 -0700

    Pycode corrections.'

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 
+"""In this module we have the function defined."""
 def say_my_name(first_name, last_name=""):
+    """
+    Prints the name based on the provided first name and last name.
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to "".
+
+    Raises:
+        TypeError: If the first_name or last_name is not a string.
+
+    Returns:
+        None
+    """
 
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
@@ -11,8 +25,4 @@ def say_my_name(first_name, last_name=""):
         print("My name is {} {}".format(first_name, last_name))
 
     else:
-        print("My name is {}".format(first_name))
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("3-say_my_name.txt")
+        print("My name is {} ".format(first_name))
