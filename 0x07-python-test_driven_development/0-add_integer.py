daf753330ac37bf485939926c8747927c93e4d7d
Sue609 <susankamau609@gmail.com>
2023-06-07 10:20:51 -0700

    Documenting the module and the function.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python3
 
+"""
+This module provides a function for adding two integers.
+"""
+
 def add_integer(a, b=98):
+    """
+    Adds two integers and returns the result.
+
+    Args:
+        a (int or float): The first integer or float.
+        b (int or float, optional): The second integer or float. Defaults to 98.
+
+    Returns:
+        int: The sum of the two integers.
+
+    Raises:
+        TypeError: If `a` or `b` is not an integer or float.
+
+    Examples:
+        >>> add_integer(5, 10)
+        15
+        >>> add_integer(2.5, 7)
+        9
+    """
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("0-add_integer.txt")
