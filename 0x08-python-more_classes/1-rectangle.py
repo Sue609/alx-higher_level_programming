107601682ce4bfd6caccf8051790d84591cf427a
Sue609 <susankamau609@gmail.com>
2023-06-07 07:12:48 -0700

    Corrections i had mismatched and raised inappropriate exceprions specifically the height.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -39,8 +39,7 @@ class Rectangle:
     def height(self, value):
         """Set the height of the rectangle."""
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
+            raise TypeError("height must be an integer")
+        if value < 0:i
+            raise ValueError("height must be >= 0")
         self.__height = value
-
