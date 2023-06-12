9c870c4f1961183bb27bb6e6decdd253f45e66f9
Sue609 <susankamau609@gmail.com>
2023-06-12 08:22:06 -0700

    Write a class Rectangle that inherits from BaseGeometry the area() method must be implemented.

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -43,14 +43,47 @@ This modeule introduces a new function.
 class Rectangle(BaseGeometry):
     """
     We have a new class that inherits from the BaseGeometry class.
+
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
     """
 
     def __init__(self, width, height):
         """
         A method that instantiates the width and the height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is less than or equal to 0.
         """
 
         self.__width = width
         self.__height = height
         self.integer_validator("width", self.__width)
         self.integer_validator("height", self.__height)
+
+    def area(self):
+        """
+        Calculates the area of the rectangle.
+
+        Returns:
+            The area of the rectangle.
+        """
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns a string representation of the rectangle.
+
+        Returns:
+            The string representation of the rectangle in the format:
+            [Rectangle] <width>/<height>
+        """
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
