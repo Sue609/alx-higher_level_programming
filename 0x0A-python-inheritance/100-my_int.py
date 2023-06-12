c99552f6041e114f1a8af9f6ed4ead07e5ea7fa4
Sue609 <susankamau609@gmail.com>
2023-06-12 10:26:38 -0700

    Write a class MyInt that inherits from int.

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module introduces a class called MyInt.
+"""
+
+
+class MyInt(int):
+    """
+    The class inherits from int. MyInt is a rebel.
+
+    MyInt has == and != operators inverted.
+    """
+
+    def __eq__(self, other):
+        """
+        Overrides the equality (==) operator.
+
+        Args:
+            others: The value to compare with.
+
+        Returns:
+            The inverted result of the equality check.
+        """
+        
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Overrides the inequality (!=) operator.
+
+        Args:
+            other: The value to compare with.
+
+        Returns:
+            The inverted result of the inequality check.
+        """
+
+        return super().__eq__(other)
