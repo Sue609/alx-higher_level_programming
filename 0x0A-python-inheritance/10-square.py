6271a169a4cda0a60c469aa294d265a673ac4088
Sue609 <susankamau609@gmail.com>
2023-06-12 11:51:11 -0700

    Correcting to the proper documentation.

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,136 +1,35 @@
 #!/usr/bin/python3
 """
-Module that introduces a class called BaeGeometry.
+This module defines the Square class.
 """
 
-
-class BaseGeometry:
-    """
-    An empty class defined.
-    """
-
-    def area(self):
-        """
-        Raises an Exception if area is not implemented.
-        """
-
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Public instance method that validates value.
-
-        Args:
-            name (str): The name of the value.
-            value: The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
-
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-"""
-This modeule introduces a new function.
-"""
-
-
-class Rectangle(BaseGeometry):
-    """
-    We have a new class that inherits from the BaseGeometry class.
-
-    Attributes:
-        __width (int): The width of the rectangle.
-        __height (int): The height of the rectangle.
-    """
-
-    def __init__(self, width, height):
-        """
-        A method that instantiates the width and the height.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-
-        Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is less than or equal to 0.
-        """
-
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
-
-    def area(self):
-        """
-        Calculates the area of the rectangle.
-
-        Returns:
-            The area of the rectangle.
-        """
-
-        return self.__width * self.__height
-
-    def __str__(self):
-        """
-        Returns a string representation of the rectangle.
-
-        Returns:
-            The string representation of the rectangle in the format:
-            [Rectangle] <width>/<height>
-        """
-
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-
-"""
-we introdece a new class inheritance.
-"""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """
-    Represents a square shape.
-
-    Attributes:
-        __size (int): The size of the square.
+    Represents a square.
     """
 
     def __init__(self, size):
         """
-        Initializes a new instance of the Square class.
-
+        Initializes a Square instance.
         Args:
             size (int): The size of the square.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than or equal to 0.
         """
-
+        self.__size = 0
+        self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-        self.integer_validator("size", self.__size)
-        super().__init__(self.__size, self.__size)
 
     def __str__(self):
         """
         Returns a string representation of the square.
-
-        Returns:
-            [Square] <size>/<size>
         """
-
         return "[Square] {}/{}".format(self.__size, self.__size)
 
     def area(self):
         """
-        Calculates and returns the area of the square.     
+        Computes and returns the area of the square.
         """
-
         return self.__size ** 2
