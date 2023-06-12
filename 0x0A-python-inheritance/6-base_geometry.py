40634d1d497929f2597f61226b962669be247fd5
Sue609 <susankamau609@gmail.com>
2023-06-12 07:17:53 -0700

    ublic instance method: def area(self): that raises an Exception with the message area() is not implemented.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module that introduces a class called BaeGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class defined.
+    """
+
+    def area(self):
+        """
+        Raises an Exception if area is not implemented.
+        """
+
+        raise Exception("area() is not implemented")
