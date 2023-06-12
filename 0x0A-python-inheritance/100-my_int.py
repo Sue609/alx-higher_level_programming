2ef767a1d29872caf6bd083f3dc3b1c027007bd3
Sue609 <susankamau609@gmail.com>
2023-06-12 11:57:47 -0700

    Correcting to the proper documentation.

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -12,12 +12,6 @@ class MyInt(int):
     def __eq__(self, other):
         """
         Overrides the equality (==) operator.
-
-        Args:
-            others: The value to compare with.
-
-        Returns:
-            The inverted result of the equality check.
         """
         
         return (super().__ne__(other))
@@ -25,12 +19,6 @@ class MyInt(int):
     def __ne__(self, other):
         """
         Overrides the inequality (!=) operator.
-
-        Args:
-            other: The value to compare with.
-
-        Returns:
-            The inverted result of the inequality check.
         """
 
         return (super().__eq__(other))
