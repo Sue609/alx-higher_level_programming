b95a00fcd36023d186276eb580f6c57f9035cfbe
Sue609 <susankamau609@gmail.com>
2023-06-14 10:11:22 -0700

    Pycodestyle modification.

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -26,7 +26,8 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {attr: getattr(seilf, attr)
+                    for attr in attrs if hasattr(self, attr)}
 
     def reload_from_json(self, json):
         """
