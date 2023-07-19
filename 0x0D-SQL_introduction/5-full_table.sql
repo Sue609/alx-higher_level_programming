09c50e409ab489dbd0731f800774e7b904926539
Sue609 <susankamau609@gmail.com>
2023-07-18 23:31:53 -0700

    Write a script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,12 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 
-SHOW CREATE TABLE 'first_table';
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'hbtn_0c_0'
+    AND TABLE_NAME = 'first_table';
