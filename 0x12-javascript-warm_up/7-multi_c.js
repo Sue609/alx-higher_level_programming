6feb4b54d1f98f725e6e87d5b4ca38444febd937
Sue609 <susankamau609@gmail.com>
2023-08-14 13:30:31 +0000

    Write a script that prints x times.

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/*
+script that prints x times.
+*/
+
+const argv = require('process').argv;
+
+if (argv.length !== 3) {
+  console.log('Missing number of occurences');
+} else {
+  const numTimes = parseInt(argv[2]);
+  if (!isNaN(numTimes)) {
+    for (let i = 0; i < numTimes; i++) {
+      console.log('C is fun');
+    }
+  } else {
+    console.log('Missing number of occurrences');
+  }
+}
