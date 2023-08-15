36b59f46add3585c215653a551e5e0d8282b6ec4
Sue609 <susankamau609@gmail.com>
2023-08-15 09:38:47 +0000

    Create an instance method called print() that prints the rectangle using the character X.

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/*
+Create an instance method called print() that prints the rectangle using the character X
+*/
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+  
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+	row += 'x';
+      }
+      console.log(row);
+    }
+}
+}
+
+module.exports = Rectangle;
