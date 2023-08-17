7e6e850fd3c20b4ce8ef92d585c1a38d54205ee7
Sue609 <susankamau609@gmail.com>
2023-08-17 11:37:47 -0700

    Your script should take 4 arguments: mysql username, mysql password, database name and state name searched (safe from MySQL injection).

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+"""
+This module introduces a new function.
+"""
+
+
+def display_value(username, password, database, nameSearch):
+    """
+    Script that displays all values in the state table.
+    """
+
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database,
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE %s "
+                "ORDER BY id ASC", (nameSearch,))
+    
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ ==  "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    nameSearch = sys.argv[4]
+
+    display_value(username, password, database, nameSearch)
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+
+def injection(username, password, database, state_name):
+    """
+    Injection to delete all records of a table.
+    """
+
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database, port=3306)
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+
+    cur.execute(query, (state_name,))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    injection(username, password, database, state_name)
