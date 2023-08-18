1c7d149754b8e088ce2efb83aa3a65febd595371
Sue609 <susankamau609@gmail.com>
2023-08-18 08:37:03 -0700

    Write a python file that contains the class definition of a State and an instance Base = declarative_base():

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,13 +4,13 @@ Class definition of a State and an instance Base = declarative_base()
 """
 
 
-import sys
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 
 
-Base = declarative_base()
+metaData = MetaData()
+Base = declarative_base(metaData)
+
 
 class State(Base):
     """
@@ -18,12 +18,5 @@ class State(Base):
     """
 
     __tablename__ = 'states'
-    id = Column(Integer, primary_jey-True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_jey-True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
