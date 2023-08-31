ea096bb9da1d7b697ebd1e2e7dd7640e42eccdd4
Sue609 <susankamau609@gmail.com>
2023-08-31 04:38:33 -0700

    Write a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response.

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # takes in a URL sends a request to that URL and displays the size of the response
-curl -s "$1" | wc -c
+curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r'
