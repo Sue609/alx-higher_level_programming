0adad751ec807522e7faf6fd5ad471c093d6c317
Sue609 <susankamau609@gmail.com>
2023-09-01 08:11:51 -0700

    Write a Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable found
+in the header of the response.
+"""
+
+import urllib.request
+import sys
+
+try:
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        x_request_id = response.getheader('X-Request-Id')
+        if x_request_id:
+            print(x_request_id)
+except urlib.error.URLError as e:
+    print(f"Eroor: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
