f8a58935a18212e58050a46c27e180b0c4f675e9
Sue609 <susankamau609@gmail.com>
2023-09-26 12:04:50 -0700

    javascript-web_scraping.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,9 +6,6 @@ const filePath = process.argv[2];
 
 const content = process.argv[3];
 
-if (!filePath || !content) {
-  process.exit(1);
-}
 fs.writeFile(filePath, content, error => {
   if (error) console.log(error);
 });
